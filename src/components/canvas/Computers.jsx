d83a76a51d39949449b3b7bb4c834ae1262e5ec6
stephane.malho <stepahen.malho@gmail.com>
2023-05-12 09:03:29 +0200

    ♻️  F09 Ball & Computers > add variable

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -33,10 +33,11 @@ const Computers = ({ isMobile }) => {
 
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
+  const maxWidth = "(max-width: 500px)";
 
   useEffect(() => {
     // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    const mediaQuery = window.matchMedia( maxWidth );
 
     // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
