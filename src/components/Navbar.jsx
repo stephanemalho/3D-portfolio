58a42c37061451e6d525656765c5ca1ba1928214
stephane.malho <stepahen.malho@gmail.com>
2023-05-26 10:48:54 +0200

    F09 > navbar > change width nav to 100vw

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary opacity-95`}
+      className={`${styles.paddingX} w-[100vw] flex items-center py-5 fixed top-0 z-20 bg-primary opacity-95`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <a
