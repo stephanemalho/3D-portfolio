075bab52adaca0699d79c0089888420cb8c9d6c1
stephane.malho <stepahen.malho@gmail.com>
2023-06-02 15:46:50 +0200

    💡 🔥 F09 > App > fix overflow-x to hidden

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ import {
   Navbar,
   Tech,
   Works,
-  //StarsCanvas,
+ //StarsCanvas,
 } from "./components";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
+      <div className="relative z-0 bg-primary overflow-x-hidden">
         <header className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
